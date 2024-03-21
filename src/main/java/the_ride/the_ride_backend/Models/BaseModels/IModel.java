9d6478e57334b84933e6c410828833dc755407b0
Odia Adodo <subzbelow@gmail.com>
2024-03-22 00:09:25 +0100

    Added base Model

---
+++ src/main/java/the_ride/the_ride_backend/Models/BaseModels/IModel.java
@@ -0,0 +1,9 @@
+package the_ride.the_ride_backend.Models.BaseModels;
+
+/*
+* Base Primary key is of type Integer, Long , Short or Guid
+*/
+public interface IModel<PrimaryKey> {
+    PrimaryKey getID();
+    void setID(PrimaryKey id);
+}
