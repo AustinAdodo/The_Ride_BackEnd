9932a03eb0accdb6318b68953d57193e5cc62fe7
Odia Adodo <subzbelow@gmail.com>
2024-03-22 00:31:02 +0100

    Application

---
+++ src/main/java/the_ride/the_ride_backend/TheRideBackEndApplication.java
@@ -0,0 +1,13 @@
+package the_ride.the_ride_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRideBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheRideBackEndApplication.class, args);
+    }
+
+}
