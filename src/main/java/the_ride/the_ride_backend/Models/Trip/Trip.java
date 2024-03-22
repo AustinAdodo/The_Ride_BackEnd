bd2529b3b15e021f6546a5bf18d8b216845bd1c6
Odia Adodo <subzbelow@gmail.com>
2024-03-22 20:33:39 +0100

    simple Updates

---
+++ src/main/java/the_ride/the_ride_backend/Models/Trip/Trip.java
@@ -0,0 +1,9 @@
+package the_ride.the_ride_backend.Models.Trip;
+
+import the_ride.the_ride_backend.Models.BaseModels.TripBaseModel;
+
+import java.util.UUID;
+
+public class Trip extends TripBaseModel<UUID> {
+
+}
---
+++ src/test/java/the_ride/the_ride_backend/TheRideBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package the_ride.the_ride_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheRideBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
