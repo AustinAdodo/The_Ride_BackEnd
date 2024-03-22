795f29ff0c67557ccf3edd82e96c2e3ac758607a
Odia Adodo <subzbelow@gmail.com>
2024-03-22 20:34:12 +0100

    Added Location Service to optimizze driver search

--- src/main/java/the_ride/the_ride_backend/Services/LocatorService.java
+++ src/main/java/the_ride/the_ride_backend/Services/LocatorService.java
@@ -1,10 +1,26 @@
 package the_ride.the_ride_backend.Services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import the_ride.the_ride_backend.Models.Driver.Driver;
+
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class LocatorService {
-    /**
-     * Mimics IReadOnly List in c# => //List.of()
+
+    /*
+     * Mimics IReadOnly List in c#, list order is maintained
      */
-    public static final List<String> SearchPriority = List.of("Town", "localGovernment", "District", "City", "Metropolis","State");
+    public static final List<String> SearchPriority = List.of("Street", "Town", "localGovernment", "District",
+            "City", "State");
+
+    //@Autowired
+    public LocatorService() {
+    }
+    public List<Driver> driversNearSpecifiedLocation(String streetName) {
+        // some logic to retrieve drivers near the specified location
+        return new ArrayList<>();
+    }
 }
