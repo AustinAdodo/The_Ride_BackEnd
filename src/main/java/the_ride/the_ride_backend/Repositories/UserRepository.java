5e59f4f573b92d58feb14784edc215ae920625b4
Odia Adodo <subzbelow@gmail.com>
2024-03-22 21:39:44 +0100

    Corrected UserRepository class

--- src/main/java/the_ride/the_ride_backend/Repositories/UserRepository.java
+++ src/main/java/the_ride/the_ride_backend/Repositories/UserRepository.java
@@ -1,11 +1,12 @@
 package the_ride.the_ride_backend.Repositories;
 
-import org.apache.catalina.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import the_ride.the_ride_backend.Models.User.Customer;
 
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<Customer, UUID> {
 }
