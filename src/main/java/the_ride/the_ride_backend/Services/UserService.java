379a144d0b8199b97e0c7b4d9c9a0587df3bf097
Odia Adodo <subzbelow@gmail.com>
2024-03-22 21:40:15 +0100

    Enhanced UserService

--- src/main/java/the_ride/the_ride_backend/Services/UserService.java
+++ src/main/java/the_ride/the_ride_backend/Services/UserService.java
@@ -3,19 +3,42 @@ package the_ride.the_ride_backend.Services;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-import the_ride.the_ride_backend.Models.User.User;
+import the_ride.the_ride_backend.Exceptions.UserNotFoundException;
+import the_ride.the_ride_backend.Models.User.Customer;
+import the_ride.the_ride_backend.Repositories.UserRepository;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
 
-    public UserService() {}
+    private final UserRepository _userRepository;
 
-    public User getCurrentLoggedInUser() {
+    public UserService(UserRepository userRepository) {
+        this._userRepository = userRepository;
+    }
+
+    public Customer disableUser(Customer customer) {
+        Optional<Customer> queriedUserOptional = _userRepository.findById(customer.getID());
+
+        // Check if the user exists
+        if (queriedUserOptional.isPresent()) {
+            Customer queriedUser = queriedUserOptional.get();
+            queriedUser.status = "Inactive";
+            _userRepository.save(queriedUser);
+            return queriedUser;
+        } else {
+            // Handle case when user is not found
+            throw new UserNotFoundException("User not found with ID: " + customer.getID());
+        }
+    }
+
+    public Customer getCurrentLoggedInUser() {
         // Get the authentication object from the security context holder
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        if (authentication != null && authentication.getPrincipal() instanceof User) {
-            return (User) authentication.getPrincipal();
+        if (authentication != null && authentication.getPrincipal() instanceof Customer) {
+            return (Customer) authentication.getPrincipal();
         } else {
             return null;
         }
