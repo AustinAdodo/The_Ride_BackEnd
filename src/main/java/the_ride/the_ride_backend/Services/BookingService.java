2f97101944fc9893f7e40890d3297c613cd59d33
Odia Adodo <subzbelow@gmail.com>
2024-03-22 20:35:22 +0100

    Update to BookingService.java

--- src/main/java/the_ride/the_ride_backend/Services/BookingService.java
+++ src/main/java/the_ride/the_ride_backend/Services/BookingService.java
@@ -1,38 +1,57 @@
 package the_ride.the_ride_backend.Services;
 
-//import jakarta.transaction.Transactional;
-//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.jpa.repository.Query;import jakarta.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import the_ride.the_ride_backend.Models.Trip.Trip;
+import the_ride.the_ride_backend.Models.User.User;
 import the_ride.the_ride_backend.Utiities.DriverSearchStatus;
-
 
 
 @Service
 public class BookingService {
     @Autowired
-    private UserService UserService;
+    private UserService _UserService;
     @Autowired
-    private DriverService DriverService;
+    private DriverService _DriverService;
+    @Autowired
+    private LocatorService _locatorService;
+    private MessagingService _MsgService;
+    private String FindStatus;
 
     @Autowired
-    public BookingService(UserService userS, DriverService drivers) {
-        this.UserService = userS;
-        this.DriverService = drivers;
+    public BookingService(UserService userS, DriverService drivers, LocatorService locatorService) {
+        this._UserService = userS;
+        this._DriverService = drivers;
+        this._locatorService = locatorService;
     }
 
     public BookingService() {
     }
 
-    public Trip SaveTrip(){
+    public Trip SaveTrip() {
         //call trip repository and save trip
         return new Trip();
     }
 
-    public void CancelBooking(){}
+    public void CancelBooking() {
+        //check if booking exists with the current user
+        //End booking
+    }
 
-    public DriverSearchStatus FindDriver(){
-        return DriverSearchStatus.Found;
+    /**
+     * Finds the closest driver and sends message asynchronously to the driver.
+     */
+    public void FindDriver() {
+        User currentUser = _UserService.getCurrentLoggedInUser();
+        var DriverList = _locatorService.driversNearSpecifiedLocation(currentUser.StreetName);
+        if(!DriverList.isEmpty()){
+            FindStatus = String.valueOf(DriverSearchStatus.Found);
+        }
+        else {
+            _MsgService.sendNotificationMessage(currentUser,"No Driver Found, Click Refresh.");
+        }
+        _MsgService.sendRequestMessage(DriverList.get(0));
     }
 }
