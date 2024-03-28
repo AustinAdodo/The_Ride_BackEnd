093e26de6a3401f186eaa5ddbc88ead8069ddb7b
Odia Adodo <subzbelow@gmail.com>
2024-03-28 21:14:55 +0100

    Update

--- src/main/java/the_ride/the_ride_backend/Models/Admin/BackEndUser.java
+++ src/main/java/the_ride/the_ride_backend/Models/Admin/BackEndUser.java
@@ -1,11 +1,31 @@
 package the_ride.the_ride_backend.Models.Admin;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import the_ride.the_ride_backend.Models.BaseModels.UserBaseModel;
 
 /*
  * For internal Users
  */
+
+@Entity
 public class BackEndUser extends UserBaseModel<Integer> {
+    @jakarta.persistence.Id
+    @Column(name = "ID", nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Integer id) {
+        this.id = id;
+    }
     public BackEndUser() {
         super("superUser");
     }
