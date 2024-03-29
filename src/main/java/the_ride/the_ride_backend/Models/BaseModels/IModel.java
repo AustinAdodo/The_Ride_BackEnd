a6d19d0a6c6c6a12569f37bde04882fe42c4739b
Odia Adodo <subzbelow@gmail.com>
2024-03-30 00:10:51 +0100

    Update

--- src/main/java/the_ride/the_ride_backend/Models/BaseModels/IModel.java
+++ src/main/java/the_ride/the_ride_backend/Models/BaseModels/IModel.java
@@ -4,6 +4,6 @@ package the_ride.the_ride_backend.Models.BaseModels;
 * Base Primary key is of type Integer, Long , Short or Guid
 */
 public interface IModel<PrimaryKey> {
-    PrimaryKey getID();
-    void setID(PrimaryKey id);
+    PrimaryKey getId();
+    void setId(PrimaryKey id);
 }
