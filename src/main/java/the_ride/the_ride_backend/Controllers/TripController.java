fcb748b1320ab2da108c4d54b8a7bab9a824674b
Odia Adodo <subzbelow@gmail.com>
2024-04-02 20:34:32 +0100

    Enhanced with Logging and used LoggerFactory

--- src/main/java/the_ride/the_ride_backend/Controllers/TripController.java
+++ src/main/java/the_ride/the_ride_backend/Controllers/TripController.java
@@ -1,5 +1,6 @@
 package the_ride.the_ride_backend.Controllers;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -10,9 +11,13 @@ import the_ride.the_ride_backend.Services.MessagePublisher;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Controller
 public class TripController {
     private final MessagePublisher messagePublisher;
+    private static final Logger logger = LoggerFactory.getLogger(TripController.class);
 
     private final ObjectMapper objectMapper;
 
@@ -29,7 +34,7 @@ public class TripController {
             String message = objectMapper.writeValueAsString(driverDetails);
             messagePublisher.sendRequestMessage(message);
         } catch (JsonProcessingException e) {
-            e.printStackTrace();
+            logger.error("Error processing acceptTrip message: ", e);
             // Handle JSON conversion error
         }
         return driverDetails;
@@ -42,7 +47,7 @@ public class TripController {
             String message = objectMapper.writeValueAsString(customerDetails);
             messagePublisher.sendRequestMessage(message);
         } catch (JsonProcessingException e) {
-            e.printStackTrace();
+            logger.error("Error processing tripRequest message: ", e);
             // Handle JSON conversion error
         }
         return customerDetails;
