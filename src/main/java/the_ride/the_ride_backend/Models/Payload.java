336bc2b7b86ec2f5cf7c283ac1843109615743d6
Odia Adodo <subzbelow@gmail.com>
2024-04-02 15:25:33 +0100

    Payload for Serialising Incoming messages

---
+++ src/main/java/the_ride/the_ride_backend/Models/Payload.java
@@ -0,0 +1,17 @@
+package the_ride.the_ride_backend.Models;
+
+public class Payload {
+
+    public String name = "Driver";
+    public String phoneNumber = "xxx";
+    public double rating = 5.0;
+    public boolean hasAccepted = true;
+    public String message = "Your Driver is on his way";
+    public String photoUrl = "";
+    public String sex = "";
+    public String type = "General Message";
+
+//    public Payload (Payload payload) {
+//
+//    }
+}
