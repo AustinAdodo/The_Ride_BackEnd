f2e064c23af7b775e3f570eef640ba05a0d8dcca
Odia Adodo <subzbelow@gmail.com>
2024-04-04 19:17:37 +0100

    Added tested Controller for Customers(Users)

---
+++ src/main/java/the_ride/the_ride_backend/Controllers/CustomerController.java
@@ -0,0 +1,40 @@
+package the_ride.the_ride_backend.Controllers;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import the_ride.the_ride_backend.Models.User.Customer;
+import the_ride.the_ride_backend.Services.UserService;
+
+import java.util.List;
+
+@RestController
+public class CustomerController {
+    private final UserService service;
+    public CustomerController(UserService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/customers")
+    public ResponseEntity<Customer> create(@RequestBody Customer customer) {
+        if (customer != null) {
+            HttpHeaders headers = new HttpHeaders();
+            this.service.add(customer);
+            headers.add(HttpHeaders.CONTENT_TYPE, "application/json; charset=UTF-8");
+            return new ResponseEntity<>(customer, headers, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(new Customer(), HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/customers")
+    public ResponseEntity<List<Customer>> getAllArticles() {
+        List<Customer> articles = this.service.getAll();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, "application/json; charset=UTF-8");
+        return new ResponseEntity<>(articles, headers, HttpStatus.OK);
+    }
+}
