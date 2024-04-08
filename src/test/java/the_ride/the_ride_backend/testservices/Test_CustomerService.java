34829ee2ae65f01a918222644ab6f584d7f3f2f9
Odia Adodo <subzbelow@gmail.com>
2024-04-08 16:50:33 +0100

    Update

--- src/test/java/the_ride/the_ride_backend/testservices/Test_CustomerService.java
+++ src/test/java/the_ride/the_ride_backend/testservices/Test_CustomerService.java
@@ -15,10 +15,9 @@ import java.util.UUID;
 @Service
 public class Test_CustomerService {
     private final Test_CustomerRepository test_customerRepository;
-    
+
     @Autowired
     public Test_CustomerService(Test_CustomerRepository testCustomerRepository) {
-
         test_customerRepository = testCustomerRepository;
     }
 
@@ -39,7 +38,7 @@ public class Test_CustomerService {
     }
 
     @Transactional
-    @Query(value = "SELECT * FROM testCustomers", nativeQuery = true)
+    @Query(value = "SELECT * FROM test_Customers", nativeQuery = true)
     public List<Test_Customer> getAll() {
         List<Test_Customer> all = new ArrayList<>();
         if (this.test_customerRepository != null) {
@@ -55,5 +54,22 @@ public class Test_CustomerService {
     @Transactional
     public Test_Customer findById(UUID id) {
         return test_customerRepository.findById(id).get();
+    }
+
+    @Transactional
+    public void UpdateCustomer(Test_Customer customer) {
+        if (this.test_customerRepository != null) {
+            test_customerRepository.save(customer);
+        }
+    }
+
+    @Transactional
+    public void remove(UUID id) {
+        try {
+            Test_Customer customer= findById(id);
+            test_customerRepository.delete(customer);
+        } catch (Exception e) {
+            throw e;
+        }
     }
 }
