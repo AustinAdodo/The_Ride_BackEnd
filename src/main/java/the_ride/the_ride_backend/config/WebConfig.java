3f01c4f9a0a1e4d568d1cae16c6564b7ab2f3b5b
Odia Adodo <subzbelow@gmail.com>
2024-04-25 13:07:44 +0100

    Moved the password encoder to SecurityConfig.java

--- src/main/java/the_ride/the_ride_backend/config/WebConfig.java
+++ src/main/java/the_ride/the_ride_backend/config/WebConfig.java
@@ -18,15 +18,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*");
     }
-
-    /**
-     * Password Encoder
-     */
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }
 
 
