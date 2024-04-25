bc1ac59867745cd1be11729246e48f1b44b4fbb7
Odia Adodo <subzbelow@gmail.com>
2024-04-25 12:18:37 +0100

    Extension of UserRepository

--- src/main/java/the_ride/the_ride_backend/Repositories/UserRepository.java
+++ src/main/java/the_ride/the_ride_backend/Repositories/UserRepository.java
@@ -1,12 +1,13 @@
 package the_ride.the_ride_backend.Repositories;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import the_ride.the_ride_backend.Models.User.Customer;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<Customer, UUID> {
+    Optional<Customer> findByUsername(String username);
 }
