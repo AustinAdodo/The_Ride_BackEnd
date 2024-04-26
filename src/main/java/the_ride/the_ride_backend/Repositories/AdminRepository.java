49a9c46238ea0ff8537214e56e8af4754fead503
Odia Adodo <subzbelow@gmail.com>
2024-04-26 12:50:38 +0100

    AdminRepository.java

--- src/main/java/the_ride/the_ride_backend/Repositories/AdminRepository.java
+++ src/main/java/the_ride/the_ride_backend/Repositories/AdminRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import the_ride.the_ride_backend.Models.Admin.BackEndUser;
 
+import java.util.Optional;
+
 @Repository
 public interface AdminRepository extends JpaRepository<BackEndUser, Integer> {
+    Optional<BackEndUser> findByUsername(String username);
 }
 
