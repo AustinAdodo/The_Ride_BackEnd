6f58de9d52d4ede6c7f8a1d0ec1f510094e160ed
Odia Adodo <subzbelow@gmail.com>
2024-04-26 12:57:00 +0100

    removed comments

--- src/main/java/the_ride/the_ride_backend/config/WebConfig.java
+++ src/main/java/the_ride/the_ride_backend/config/WebConfig.java
@@ -1,14 +1,13 @@
 package the_ride.the_ride_backend.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
@@ -19,14 +18,3 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*");
     }
 }
-
-
-//@Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("*") // Allows all origins for testing and demonstrations
-//                .allowedMethods("*") // Allows all methods for testing and demonstrations
-//                .allowedHeaders("*")
-//                .allowCredentials(true);
-//    }
-////.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
