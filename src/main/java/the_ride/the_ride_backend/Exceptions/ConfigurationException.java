7453df635fbf809faa052431ec767c25ba39a3d0
Odia Adodo <subzbelow@gmail.com>
2024-04-27 22:31:29 +0100

    A class that handles custom exception for configurations

---
+++ src/main/java/the_ride/the_ride_backend/Exceptions/ConfigurationException.java
@@ -0,0 +1,12 @@
+package the_ride.the_ride_backend.Exceptions;
+
+public class ConfigurationException extends Exception {
+    public ConfigurationException(String message) {
+        super(message);
+    }
+
+    public ConfigurationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
