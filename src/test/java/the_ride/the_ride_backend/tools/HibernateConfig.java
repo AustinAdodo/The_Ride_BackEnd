37711892dc4ab8ffb6e9a42c3ebe11b7908a4a38
Odia Adodo <subzbelow@gmail.com>
2024-04-28 14:37:35 +0100

    Added HibernateConfig.java nd TransactionEventListenerIntegrator.java for in-depth debugging.

--- src/test/java/the_ride/the_ride_backend/tools/HibernateConfig.java
+++ src/test/java/the_ride/the_ride_backend/tools/HibernateConfig.java
@@ -6,25 +6,25 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class HibernateConfig {
-    @Autowired
-    private EntityManagerFactory entityManagerFactory;
-
-    @Bean
-    public Interceptor hibernateInterceptor() {
-        return new EntityInterceptor();
-    }
-
-    @Bean
-    public SessionFactory sessionFactory() {
-        // Assumes you have a LocalContainerEntityManagerFactoryBean that creates EntityManagerFactory
-        if (entityManagerFactory.unwrap(SessionFactory.class) == null) {
-            throw new NullPointerException("factory is not a hibernate factory");
-        }
-        SessionFactory sessionFactory = entityManagerFactory.unwrap(SessionFactory.class);
-        sessionFactory.withOptions().interceptor(hibernateInterceptor());
-        return sessionFactory;
-    }
-}
+//
+//@Configuration
+//public class HibernateConfig {
+//    @Autowired
+//    private EntityManagerFactory entityManagerFactory;
+//
+//    @Bean
+//    public Interceptor hibernateInterceptor() {
+//        return new EntityInterceptor();
+//    }
+//
+//    @Bean
+//    public SessionFactory sessionFactory() {
+//        // Assumes you have a LocalContainerEntityManagerFactoryBean that creates EntityManagerFactory
+//        if (entityManagerFactory.unwrap(SessionFactory.class) == null) {
+//            throw new NullPointerException("factory is not a hibernate factory");
+//        }
+//        SessionFactory sessionFactory = entityManagerFactory.unwrap(SessionFactory.class);
+//        sessionFactory.withOptions().interceptor(hibernateInterceptor());
+//        return sessionFactory;
+//    }
+//}
--- src/test/java/the_ride/the_ride_backend/tools/TransactionEventListenerIntegrator.java
+++ src/test/java/the_ride/the_ride_backend/tools/TransactionEventListenerIntegrator.java
@@ -9,40 +9,40 @@ import org.hibernate.persister.entity.EntityPersister;
 import org.hibernate.service.spi.SessionFactoryServiceRegistry;
 import org.jetbrains.annotations.NotNull;
 
-public class TransactionEventListenerIntegrator implements Integrator {
-
-    /**
-     * a custom insert listener is registered in eventListenerRegistry.appendListeners
-     * Other Event Listeners include  POST_COMMIT_UPDATE and POST_COMMIT_DELETE.
-     *  Spring’s TransactionSynchronizationManager allows for detailed transaction synchronization callbacks
-     *  that are aware of the transaction context, including successful commit and rollback scenarios.
-     */
-
-    @Override
-    public void integrate(@NotNull Metadata metadata, @NotNull SessionFactoryImplementor sessionFactory,
-                          SessionFactoryServiceRegistry serviceRegistry) {
-        final EventListenerRegistry eventListenerRegistry = serviceRegistry.getService(EventListenerRegistry.class);
-        assert eventListenerRegistry != null;
-        eventListenerRegistry.appendListeners(EventType.POST_COMMIT_INSERT, new PostCommitInsertEventListener() {
-            @Override
-            public boolean requiresPostCommitHandling(EntityPersister entityPersister) {
-                return false;
-            }
-
-            @Override
-            public void onPostInsertCommitFailed(PostInsertEvent postInsertEvent) {
-            }
-
-            @Override
-            public void onPostInsert(PostInsertEvent event) {
-                System.out.println("Entity inserted: " + event.getEntity());
-            }
-        });
-    }
-
-    @Override
-    public void disintegrate(@NotNull SessionFactoryImplementor sessionFactory,
-                             @NotNull SessionFactoryServiceRegistry serviceRegistry) {
-        // This method can be used to clean up any resources tied to the SessionFactory
-    }
-}
+//public class TransactionEventListenerIntegrator implements Integrator {
+//
+//    /**
+//     * a custom insert listener is registered in eventListenerRegistry.appendListeners
+//     * Other Event Listeners include  POST_COMMIT_UPDATE and POST_COMMIT_DELETE.
+//     *  Spring’s TransactionSynchronizationManager allows for detailed transaction synchronization callbacks
+//     *  that are aware of the transaction context, including successful commit and rollback scenarios.
+//     */
+//
+//    @Override
+//    public void integrate(@NotNull Metadata metadata, @NotNull SessionFactoryImplementor sessionFactory,
+//                          SessionFactoryServiceRegistry serviceRegistry) {
+//        final EventListenerRegistry eventListenerRegistry = serviceRegistry.getService(EventListenerRegistry.class);
+//        assert eventListenerRegistry != null;
+//        eventListenerRegistry.appendListeners(EventType.POST_COMMIT_INSERT, new PostCommitInsertEventListener() {
+//            @Override
+//            public boolean requiresPostCommitHandling(EntityPersister entityPersister) {
+//                return false;
+//            }
+//
+//            @Override
+//            public void onPostInsertCommitFailed(PostInsertEvent postInsertEvent) {
+//            }
+//
+//            @Override
+//            public void onPostInsert(PostInsertEvent event) {
+//                System.out.println("Entity inserted: " + event.getEntity());
+//            }
+//        });
+//    }
+//
+//    @Override
+//    public void disintegrate(@NotNull SessionFactoryImplementor sessionFactory,
+//                             @NotNull SessionFactoryServiceRegistry serviceRegistry) {
+//        // This method can be used to clean up any resources tied to the SessionFactory
+//    }
+//}
