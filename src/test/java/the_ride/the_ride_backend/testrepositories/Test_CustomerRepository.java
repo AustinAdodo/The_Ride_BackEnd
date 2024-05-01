e52e2d430729a0738e320b825031d3020ef6363b
Odia Adodo <subzbelow@gmail.com>
2024-05-01 22:35:41 +0100

    Added delete all quicker transactional constraint

--- src/test/java/the_ride/the_ride_backend/testrepositories/Test_CustomerRepository.java
+++ src/test/java/the_ride/the_ride_backend/testrepositories/Test_CustomerRepository.java
@@ -1,10 +1,11 @@
 package the_ride.the_ride_backend.testrepositories;
 
+import org.slf4j.Logger;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import the_ride.the_ride_backend.testmodels.Test_Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
 
 import java.util.UUID;
 
@@ -15,6 +16,10 @@ import static org.slf4j.LoggerFactory.getLogger;
  */
 @Repository
 public interface Test_CustomerRepository extends JpaRepository<Test_Customer, UUID> {
+    @Modifying
+    @Query(value = "DELETE FROM test_Customers WHERE id IN (SELECT TOP 20 id FROM test_Customers ORDER BY id)", nativeQuery = true)
+    void deleteTop20Customers();
+
     static Logger logger() {
         final class LogHolder {
             private static final Logger LOGGER = getLogger(Test_CustomerRepository.class);
