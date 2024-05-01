a9b996bf04eea75884d1133271481024dadc72d1
Odia Adodo <subzbelow@gmail.com>
2024-05-01 22:37:00 +0100

    Configured a test security class for tests

---
+++ src/test/java/Config/TestSecurityConfig.java
@@ -0,0 +1,22 @@
+package Config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@TestConfiguration
+@EnableWebSecurity
+public class TestSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/test_customers/**").permitAll()); // Public paths
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+}
+
