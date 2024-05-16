5c0246d2a66c2a86b830ca3284a1eae764ff0910
Odia Adodo <subzbelow@gmail.com>
2024-05-16 11:59:44 +0100

    Update to Active Profiles with environment variables

--- src/main/java/the_ride/the_ride_backend/TheRideBackEndApplication.java
+++ src/main/java/the_ride/the_ride_backend/TheRideBackEndApplication.java
@@ -1,13 +1,7 @@
 package the_ride.the_ride_backend;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
-
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class TheRideBackEndApplication {
