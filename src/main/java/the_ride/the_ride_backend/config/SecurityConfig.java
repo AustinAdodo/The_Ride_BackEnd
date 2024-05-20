558c9e9ceca87543d89a285a9669fadf3d3dbfda
Odia Adodo <subzbelow@gmail.com>
2024-05-20 10:45:10 +0100

    Updated docker file to ensure uber file The_Ride_BackEnd-0.0.1-SNAPSHOT-shaded.jar s located

--- src/main/java/the_ride/the_ride_backend/config/SecurityConfig.java
+++ src/main/java/the_ride/the_ride_backend/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package the_ride.the_ride_backend.config;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -11,7 +10,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
