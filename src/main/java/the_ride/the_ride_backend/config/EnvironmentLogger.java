155469f533846917f39152be6a6e07b67dcaa481
Odia Adodo <subzbelow@gmail.com>
2024-05-21 07:51:53 +0100

    correction to docker image

---
+++ src/main/java/the_ride/the_ride_backend/config/EnvironmentLogger.java
@@ -0,0 +1,30 @@
+package the_ride.the_ride_backend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EnvironmentLogger {
+
+    @Value("${spring.datasource.url:Not Found}")
+    private String datasourceUrl;
+
+    @Value("${spring.datasource.username:Not Found}")
+    private String datasourceUsername;
+
+    @Value("${spring.datasource.password:Not Found}")
+    private String datasourcePassword;
+
+    @Bean
+    public CommandLineRunner logEnvironmentVariables() {
+        return args -> {
+            System.out.println("===== Environment Variables =====");
+            System.out.println("spring.datasource.url: " + datasourceUrl);
+            System.out.println("spring.datasource.username: " + datasourceUsername);
+            System.out.println("spring.datasource.password: " + datasourcePassword);
+            System.out.println("=================================");
+        };
+    }
+}
